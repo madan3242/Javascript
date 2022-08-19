fe89380cc9be4e09302fcef29d6c60b6cb523006
madan3242 <madangowda3242@gmail.com>
2022-08-19 12:35:12 +0530

    Ascending Descending

---
+++ Javascript Examples/Conditional Statements/11AscendingOrder.js
@@ -0,0 +1,18 @@
+var arr = [6, 4, 7];
+
+var temp = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }  
+}
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    
+}
