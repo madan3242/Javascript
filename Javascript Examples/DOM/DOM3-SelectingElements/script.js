8db98db6847719605b88f3d39b655d6c0a70f45f
madan3242 <madangowda3242@gmail.com>
2022-09-09 11:12:41 +0530

    DOM

---
+++ Javascript Examples/DOM/DOM3-SelectingElements/script.js
@@ -0,0 +1,22 @@
+document.getElementsByTagName('li').style.color = 'red';
+
+document.getElementsByClassName('list').style.color = 'red';
+
+
+
+document.getElementById
+
+document.getElementsByClassName
+
+document.getElementsByTagName
+
+document.querySelector()
+
+
+document.querySelector('li a');
+
+document.querySelector('h1#title');
+
+document.querySelectorAll('#list .item');
+
+document.querySelectorAll('#list .item')[1];
