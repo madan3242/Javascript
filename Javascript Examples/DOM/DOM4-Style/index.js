c9ca1f1872b90de372dd4b297ebc3a196fc24534
madan3242 <madangowda3242@gmail.com>
2022-09-09 11:36:01 +0530

    DOM style

---
+++ Javascript Examples/DOM/DOM4-Style/index.js
@@ -0,0 +1,20 @@
+document.querySelector('#title').style.color = 'blue';
+document.querySelector('#title').style.fontSize = '10em';
+document.querySelector('#title').style.paddingLeft = '2em';
+
+document.querySelector('button').style.backgroundColor = 'yellow';
+
+
+document.querySelector('button').classList;
+
+// document.querySelector('button').classList.add('invisible');
+
+// document.querySelector('button').classList.remove('invisible');
+
+// document.querySelector('button').classList.toggle('invisible');
+
+document.querySelector('h2').classList.add('huge');
+
+// document.querySelector('h2').classList.remove('huge');
+
+
