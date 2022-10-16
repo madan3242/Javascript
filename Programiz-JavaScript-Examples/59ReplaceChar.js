b5b31fea069003c2d1f753c291d845217f40f0f5
madan3242 <madangowda3242@gmail.com>
2022-10-16 20:59:10 +0530

    Js

---
+++ Programiz-JavaScript-Examples/59ReplaceChar.js
@@ -0,0 +1,9 @@
+//Replace All Instances Of Character Using Built-in Methods
+
+const string = "Learning Javascript Program";
+
+const splitString = string.split('a');
+
+const result = splitString.join('A');
+
+console.log(result);
---
+++ Programiz-JavaScript-Examples/60MultilineString.js
@@ -0,0 +1,22 @@
+// JavaScript Program to Create Multiline Strings
+
+const message = 'This is a long message\n' + 
+    'that spans across multiple lines\n' + 
+    'in the code.'
+
+console.log(message);
+
+// program to create a multiline strings using the \ operator
+const message2 = 'This is a long message\n \
+that spans across multiple lines\n \
+in the code.'
+
+console.log(message2);
+
+// program to create a multiline strings using the template literal
+
+const message3 = `This is a long message
+that spans across multiple lines
+in the code.`
+
+console.log(message3);
